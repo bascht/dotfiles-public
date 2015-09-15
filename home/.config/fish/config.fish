0cf199c2c3f62fa8e693bbe7015eccb5d011c37b
Sebastian Schulze <github.com@bascht.com>
2015-09-15 09:37:50 +0200

    Make i3 behave in HiDPI mode. :]

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -31,6 +31,11 @@ alias er "e --eval '(writeroom-mode)'"
 function scale
     /usr/bin/gsettings set org.gnome.desktop.interface scaling-factor $argv;
     /usr/bin/gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gdk/WindowScalingFactor': <$argv>}";
+    if math "$argv == 1"
+            /usr/bin/xrandr --dpi 96
+    else
+            /usr/bin/xrandr --dpi 144
+    end
 end
 
 # Initialize fasd
