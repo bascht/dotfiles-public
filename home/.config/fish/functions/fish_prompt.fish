3d0e9d486e9f140ea7a4c36bdc64d60bc55f8efd
Sebastian Schulze <github.com@bascht.com>
2016-01-21 10:43:54 +0100

    [fish] My Prompt needs moar animals!

---
+++ home/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,14 @@
+function fish_prompt
+        switch (hostname -s)
+                case tdhtest
+                        set logo "🐮"
+                case tivoli stormcage
+                        set logo "🐎"
+                case '*'
+                        set logo "?"
+        end
+        set_color blue
+        echo -n $logo (pwd)
+        echo -n " \$ "
+        set_color normal
+end
