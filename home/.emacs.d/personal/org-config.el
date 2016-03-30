666e89c162e16fff916f206715f90e6988f72719
Sebastian Schulze <github.com@bascht.com>
2016-03-30 08:41:30 +0200

    [emacs] Add Capture template for TODO + Rubyshift

--- home/.emacs.d/personal/org-config.el
+++ home/.emacs.d/personal/org-config.el
@@ -10,5 +10,12 @@
 (setq org-agenda-files (quote ("~/ownCloud/clientsync/Zettelkasten")))
 (setq org-default-notes-file "~/ownCloud/clientsync/Zettelkasten/refile.org")
 
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/ownCloud/clientsync/Zettelkasten/Todo.org" "Inbox")
+         "* TODO %?\n  %i\n  %a")
+        ("r" "RubyShift" entry (file+headline "~/ownCloud/clientsync/Zettelkasten/Rubyshift.org" "Unsorted")
+         "* %?\nEntered on %U\n  %i\n  %a")))
+
+
 (provide 'org-config)
 ;;; org-config.el ends here
