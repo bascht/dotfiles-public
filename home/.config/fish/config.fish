cf204c323390e69a1a23b0b998cf2063f35c78ee
Sebastian Schulze <github.com@bascht.com>
2017-02-17 11:51:26 +0100

    [omf] Correctly source omf *before* install

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,11 @@
+# Source OMF
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
+
 set fish_greeting
 set -x EDITOR 'emacsclient -t'
 set -x GIT_EDITOR $EDITOR
