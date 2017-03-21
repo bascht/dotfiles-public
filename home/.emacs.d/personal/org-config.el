f65907059d988c3455a0925385020efe0c2e8f90
Sebastian Schulze <github.com@bascht.com>
2017-03-21 14:55:12 +0100

    [emacs] Adjust to new (no longer symlinked) Org Folder

--- home/.emacs.d/personal/org-config.el
+++ home/.emacs.d/personal/org-config.el
@@ -6,14 +6,14 @@
 
 (use-package org)
 
-(setq org-directory "~/ownCloud/clientsync/Zettelkasten")
-(setq org-agenda-files (quote ("~/ownCloud/clientsync/Zettelkasten")))
-(setq org-default-notes-file "~/ownCloud/clientsync/Zettelkasten/refile.org")
+(setq org-directory "~/Documents/Zettelkasten")
+(setq org-agenda-files (quote ("~/Documents/Zettelkasten")))
+(setq org-default-notes-file "~/Documents/Zettelkasten/refile.org")
 
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/ownCloud/clientsync/Zettelkasten/Todo.org" "Inbox")
+      '(("t" "Todo" entry (file+headline "~/Documents/Zettelkasten/Todo.org" "Inbox")
          "* TODO %?\n  %i\n  %a")
-        ("r" "RubyShift" entry (file+headline "~/ownCloud/clientsync/Zettelkasten/Rubyshift.org" "Unsorted")
+        ("r" "RubyShift" entry (file+headline "~/Documents/Zettelkasten/Rubyshift.org" "Unsorted")
          "* %?\nEntered on %U\n  %i\n  %a")))
 
 
