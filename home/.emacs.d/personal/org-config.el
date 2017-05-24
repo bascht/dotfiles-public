1825455d53bc9bddac1418f0c08065db055f63eb
Sebastian Schulze <github.com@bascht.com>
2017-05-24 21:00:23 +0200

    [emacs] Add a nifty org capture shortcut that keeps the actual Email
    Courtesy of http://pragmaticemacs.com/emacs/master-your-inbox-with-mu4e-and-org-mode/

--- home/.emacs.d/personal/org-config.el
+++ home/.emacs.d/personal/org-config.el
@@ -5,6 +5,7 @@
 ;;; Code:
 
 (use-package org)
+(use-package org-mu4e)
 
 (setq org-directory "~/Documents/Zettelkasten")
 (setq org-agenda-files (quote ("~/Documents/Zettelkasten")))
@@ -13,12 +14,16 @@
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline "~/Documents/Zettelkasten/Todo.org" "Inbox")
          "* TODO %?\n  %i\n  %a")
-        ("r" "RubyShift" entry (file+headline "~/Documents/Zettelkasten/Rubyshift.org" "Unsorted")
+        ("m" "MailTodo" entry (file+headline "~/Documents/Zettelkasten/Todo.org" "Inbox")
+         "* TODO [#A] %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n%a\n")
+        ("r" "RubyShift" entry (file+headline "~/Documents/Zettelkasten/Projects.org" "RubyShift")
          "* %?\nEntered on %U\n  %i\n  %a")))
 
 (setq org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
 (setq org-completion-use-ido t)
 
+;;store link to message if in header view, not to header query
+(setq org-mu4e-link-query-in-headers-mode nil)
 
 (provide 'org-config)
 ;;; org-config.el ends here
