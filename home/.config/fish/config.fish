cb9c8ad5a4b4ecc9c997e65cd84bf3b756dfa5d2
Sebastian Schulze <github.com@bascht.com>
2017-11-13 10:32:08 +0100

    [fish] Add letter-generator "tooling".

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -40,6 +40,7 @@ alias docker-rm-dangling "docker rmi (docker images -q -f dangling=true)"
 alias dco "docker-compose"
 alias emacseval "emacs -batch -l ~/.emacs.d/init.el -eval '(org-batch-agenda "a")'"
 alias dry "docker run -it -v /var/run/docker.sock:/var/run/docker.sock moncho/dry"
+alias letterup "cp -a ~/Documents/Personal/Brief-Vorlage/2017-LaTeX/ \$argv; and cd"
 # Fire up RVM
 set -e GEM_PATH; set -e GEM_HOME
 
