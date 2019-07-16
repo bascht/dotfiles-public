6c6cfb22a29d604cb32632448efe8cc89132db58
Sebastian Schulze <github.com@bascht.com>
2019-07-16 14:14:39 +0200

    [org] Fix indention of org-config

--- home/.spacemacs.d/public/org-config.el
+++ home/.spacemacs.d/public/org-config.el
@@ -13,9 +13,9 @@
 (setq org-directory "~/Documents/Zettelkasten")
 (setq org-agenda-files '("~/Documents/Zettelkasten"))
 (setq org-default-notes-file "~/Documents/Zettelkasten/refile.org")
-(setq org-agenda-sticky t)
+(setq org-agenda-sticky nil)
 (setq org-agenda-ndays 1)
-(setq org-agenda-span 3)
+(setq org-agenda-span 1)
 (setq org-log-done t)
 (setq org-agenda-file-regexp (format-time-string "\\`[^.].*\\.org\\'\\|\\`%Y%m[0-9]+\\'"))
 (setq org-agenda-hide-tags-regexp "presents")
@@ -28,7 +28,7 @@
 (setq org-archive-location "%s_archive::")
 (setq org-deadline-warning-days 5)
 (setq org-scheduled-delay-days 0)
-;(setq org-duration-format (quote (("h" . t) (special . 2))))
+                                        ;(setq org-duration-format (quote (("h" . t) (special . 2))))
 (setq org-confirm-elisp-link-function nil)
 (setq org-icalendar-alarm-time 120)
 (setq org-icalendar-combined-agenda-file "~/Nextcloud/OrgExport/Org.ics")
@@ -45,6 +45,8 @@
          "* TODO %?\n  %i")
         ("m" "MailTodo" entry (file "~/Documents/Zettelkasten/Todo.org")
          "* TODO [#A] %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n%a\n")
+        ("c" "CustomerIssue" entry (file "~/Documents/Zettelkasten/Todo.org")
+         "* TODO [#A] is-issue%?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n")
         ("r" "RubyShift" entry (file+headline "~/Documents/Zettelkasten/Projects.org" "RubyShift")
          "* %?\nEntered on %U\n  %i\n  %a")))
 
@@ -53,6 +55,19 @@
 (setq org-outline-path-complete-in-steps nil)
 (setq org-refile-allow-creating-parent-nodes 'confirm)
 (setq org-clock-sound t)
+(setq org-clock-out-remove-zero-time-clocks t)
+(setq org-clock-persist t)
+
+(setq org-agenda-start-with-clockreport-mode t)
+(setq org-clock-report-include-clocking-task t)
+(setq org-return-follows-link t)
+
+(setq org-agenda-skip-deadline-if-done t)
+(setq org-agenda-skip-scheduled-if-done t)
+(setq org-agenda-skip-scheduled-if-deadline-is-shown t)
+(setq org-columns-default-format
+      "%80ITEM(Task) %10Effort(Effort){:} %10CLOCKSUM")
+
 (setq org-show-notification-handler "notify-send")
 
 (setq org-tag-faces (quote (
@@ -60,7 +75,7 @@
                             ("waiting" . "blue")
                             )))
 
-;(setq org-agenda-current-time ((t (:inherit org-time-grid :foreground "orange red"))))
+                                        ;(setq org-agenda-current-time ((t (:inherit org-time-grid :foreground "orange red"))))
 
 ;;store link to message if in header view, not to header query
 (setq org-mu4e-link-query-in-headers-mode nil)
@@ -69,37 +84,21 @@
 (setq org-alert-notification-title "OrgMode")
 (setq org-plantuml-jar-path "/home/bascht/bin/plantuml.jar")
 
-;; (org-babel-do-load-languages
-;;  (quote org-babel-load-languages)
-;;  (quote ((emacs-lisp . t)
-;;          (dot . t)
-;;          (ditaa . t)
-;;          (R . t)
-;;          (python . t)
-;;          (ruby . t)
-;;          (gnuplot . t)
-;;          (clojure . t)
-;;          (sh . t)
-;;          (ledger . t)
-;;          (org . t)
-;;          (plantuml . t)
-;;          (latex . t))))
-
 (setq org-modules
-  (quote
-   (org-bbdb
-    org-bibtex
-    org-docview
-    org-gnus
-    org-habit
-    org-info
-    org-irc
-    org-mhe
-    org-habit
-    org-checklist
-    org-rmail
-    org-mouse
-    org-w3m)))
+      (quote
+       (org-bbdb
+        org-bibtex
+        org-docview
+        org-gnus
+        org-habit
+        org-info
+        org-irc
+        org-mhe
+        org-habit
+        org-checklist
+        org-rmail
+        org-mouse
+        org-w3m)))
 
 
 (setq org-feed-alist
@@ -117,34 +116,56 @@
 (setq org-ellipsis "  ")
 (setq org-pretty-entities t)
 
+(setq org-habit-today-glyph ?‖)
+(setq org-habit-completed-glyph ?🗸)
+(setq org-habit-show-habits-only-for-today nil)
+(setq org-habit-graph-column 60)
+(setq org-habit-following-days 3)
+
+
 (setq org-agenda-custom-commands
-      '(("l" "Open loops" agenda ""
-         ((org-agenda-start-day "-2d")
-          (org-agenda-span 4)
+      '(("l" "Open loops" 
+         ((agenda ""))
+         ((org-agenda-start-day "-1d")
+          ;; (org-agenda-span 'week)
+          (org-agenda-show-log nil)
+          (org-agenda-ndays 2)
+          (tags-todo "-imported")
           (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
           (org-agenda-start-with-log-mode '(closed clock state) )))
+        ("D" "Old and DONE items" 
+         ((agenda ""))
+         ((org-agenda-start-day "-1d")
+          ;; (org-agenda-span 'week)
+          (org-agenda-show-log nil)
+          (org-agenda-ndays 2)
+          (tags-todo "-imported")
+          (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo))
+          (org-agenda-start-with-log-mode '(closed clock state) )))
         ("h" "Daily habits"
-           ((agenda ""))
-           ((org-agenda-show-log t)
-            (org-agenda-ndays 7)
-            (org-agenda-log-mode-items '(state))
-            (org-agenda-skip-function '(org-agenda-skip-entry-if 'notregexp ":DAILY:"))))
+         ((agenda ""))
+         ((org-agenda-show-log t)
+          (org-agenda-ndays 7)
+          (org-agenda-log-mode-items '(state))
+          (org-agenda-skip-function '(org-agenda-skip-entry-if 'notregexp ":DAILY:"))))
         ("u" "Super view"
-           ((agenda "" ((org-super-agenda-groups
-                         '((:name "Today"
-                                  :time-grid t)))))
-            (todo "" ((org-agenda-overriding-header "Projects")
-                      (org-super-agenda-groups
-                       '((auto-category t))                     )))
-            (todo "" ((org-agenda-overriding-header "Projects")
-                      (org-super-agenda-groups
-                       '((:name none  ; Disable super group header
-                                :children todo)
-                         (:discard (:anything t))))))))
+         ((agenda "" ((org-super-agenda-groups
+                       '((:name "Today"
+                                :time-grid t)))))
+          (todo "" ((org-agenda-overriding-header "Other")
+                    (org-super-agenda-groups
+                     '((auto-category t))                     )))
+          (todo "" ((org-agenda-overriding-header "Grouped")
+                    (org-super-agenda-groups
+                     '((:name none  ; Disable super group header
+                              :children todo)
+                       (:discard (:anything t))))))))
         ("c" "Mega Agenda" agenda
          (org-super-agenda-mode)
          ((org-super-agenda-groups
            '(
+             (:name "Important"
+                    :priority "A")
              (:name "Next Items"
                     :tag ("NEXT" "outbox"))
              (:name "Immersive + Deep"
@@ -152,35 +173,34 @@
              (:name "Process + Shallow"
                     :time-grid t
                     :tag ("@process" "@shallow"))
-             (:name "Important"
-                    :priority "A")
              (:name "Quick Picks"
                     :effort< "0:30"
                     )
              (:priority<= "B"
                           :scheduled future
                           :order 1)))
-         (org-agenda nil "a"))
-        ("d" "Sort during Daily" agenda
-         (org-super-agenda-mode)
-         (setq org-agenda-span 1)
-         ((org-super-agenda-groups
-           '(
-             (:name "Immersive + Deep"
-                    :tag ("@immersive" "@deep"))
-             (:name "Immersive + Shallow"
-                    :tag ("@immersive" "@shallow"))
-             (:name "Process + Deep"
-                    :tag ("@process" "@deep"))
-             (:name "Process + Shallow"
-                    :tag ("@process" "@shallow"))
-             (:name "Quick Picks"
-                    :effort< "0:30"
-                    )
-             (:priority<= "B"
-                          :scheduled future
-                          :order 1))))
-         (org-agenda nil "a")))))
+          )
+         ("d" "Sort during Daily" agenda
+          (org-super-agenda-mode)
+          (setq org-agenda-span 1)
+          ((org-super-agenda-groups
+            '(
+              (:name "Immersive + Deep"
+                     :tag ("@immersive" "@deep"))
+              (:name "Immersive + Shallow"
+                     :tag ("@immersive" "@shallow"))
+              (:name "Process + Deep"
+                     :tag ("@process" "@deep"))
+              (:name "Process + Shallow"
+                     :tag ("@process" "@shallow"))
+              (:name "Quick Picks"
+                     :effort< "0:30"
+                     )
+              (:priority<= "B"
+                           :scheduled future
+                           :order 1))))
+          (org-agenda nil "a"))
+         )))
 
 (setq org-tag-alist '(
                       (:startgroup . nil)
@@ -203,28 +223,28 @@
                       ("next" . ?N)))
 
 (setq org-bullets-bullet-list
-      '("◉" "◎" "⚫" "○" "►" "◇"))
+      '("⊶" "⊷"))
 
-; Daily Review
+                                        ; Daily Review
 (defun my-daily-review ()
   (interactive)
   (find-file "/home/bascht/Documents/Zettelkasten/Todo.org")
   (goto-char (org-find-exact-headline-in-buffer "Daily Review"))
   (org-narrow-to-subtree)
+  (org-cycle-hide-drawers 'all)
   (search-forward "[ ]"))
 
-; Recent loops callable for daily review
+                                        ; Recent loops callable for daily review
 (defun my-org-agenda-recent-open-loops ()
   (interactive)
   (let ((org-agenda-start-with-log-mode t)
+        (org-agenda-span 'day)
         (org-agenda-use-time-grid nil))
     (org-agenda nil "l")
     (beginend-org-agenda-mode-goto-beginning)))
 
 (add-to-list 'org-global-properties
-             '("Effort_ALL". "0:05 0:15 0:30 1:00 2:00 3:00 4:00"))
-
-;(spacemacs/toggle-mode-line-org-clock-on)
+             '("Effort_ALL". "0:05 0:10 0:15 0:30 1:00 2:00 3:00 4:00"))
 
 (provide 'org-config)
 ;;; org-config.el ends here
