1388e76414765b759e7aadef3ddf0d5ea310bd59
Sebastian Schulze <sebastian.schulze@alfatraining.de>
2021-09-22 10:14:03 +0200

    [nix] Span two different virtual video devices for alfaview

--- dot_config/nixos/configuration.nix
+++ dot_config/nixos/configuration.nix
@@ -17,7 +17,7 @@
   boot.kernelModules = [ "kvm-amd" "v4l2loopback" ];
   boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
   boot.extraModprobeConfig = ''
-    options v4l2loopback video_nr=8 card_label="Wayland" exclusive_caps=1
+    options v4l2loopback devices=2 video_nr=8,9 card_label="Wayland 8,Wayland 9" exclusive_caps=1
   '';
 
   boot.loader.grub = {
