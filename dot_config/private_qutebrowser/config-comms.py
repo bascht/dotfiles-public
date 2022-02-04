fd6b13fe3b0885f9c1abcc826ceaf7129ef29851
Sebastian Schulze <sebastian.schulze@alfatraining.de>
2022-02-04 20:57:30 +0100

    [bin] Add browser-comms launcher to wrap all those Slacks

---
+++ dot_config/private_qutebrowser/config-comms.py
@@ -0,0 +1,8 @@
+config.source("config.py")
+c.statusbar.show = "in-mode"
+c.tabs.show = "multiple"
+c.fonts.web.family.sans_serif = "IBM Plex Sans"
+c.fonts.web.family.standard = "IBM Plex Sans"
+c.fonts.web.family.serif = "IBM Plex Sans"
+c.fonts.web.family.cursive = "IBM Plex Sans"
+c.fonts.web.family.fixed = "IBM Plex Sans"
