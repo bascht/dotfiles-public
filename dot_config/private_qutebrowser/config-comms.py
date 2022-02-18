2d463a49273e8d030466ed35c567a5ccb7a35db3
Sebastian Schulze <github.com@bascht.com>
2022-02-18 14:27:28 +0100

    [qutebrowser] Let qutebrowser-comms handle tabs as windows

--- dot_config/private_qutebrowser/config-comms.py
+++ dot_config/private_qutebrowser/config-comms.py
@@ -1,6 +1,7 @@
 config.source("config.py")
 c.statusbar.show = "in-mode"
 c.tabs.show = "multiple"
+c.tabs.tabs_are_windows = True
 c.fonts.web.family.sans_serif = "IBM Plex Sans"
 c.fonts.web.family.standard = "IBM Plex Sans"
 c.fonts.web.family.serif = "IBM Plex Sans"
