bfed4645ed5d1eb5753e009695b70f279479dfed
Sebastian Schulze <sebastian.schulze@alfatraining.de>
2022-07-14 17:06:28 +0200

    [fish] Don't right away hop into `kubens` when selecting a cluster

--- dot_config/fish/conf.d/bascht.shortcuts.fish
+++ dot_config/fish/conf.d/bascht.shortcuts.fish
@@ -6,7 +6,7 @@ bind -M default \cn down-or-search
 bind \a "cd (z -l | sort -rn | cut -c 12- | fzf --query=(commandline --current-token) --no-sort --exact); commandline -f repaint"
 
 # Kubeshizzle
-bind \egkk "_kubeconfig|source; and kubectx; and kubens; commandline -f repaint"
+bind \egkk "_kubeconfig|source; and kubectx; commandline -f repaint"
 bind \egkx "kubectx; commandline -f repaint"
 bind \egkn "kubens; commandline -f repaint"
 bind \egkp "kgp; commandline -f repaint"
