4b8a6215c69651e80587056b53f794d2c8655f6b
Sebastian Schulze <sebastian.schulze@alfatraining.de>
2022-08-03 22:52:45 +0200

    [nix] Increase number of items in the clipman history to 1000

--- dot_config/nixpkgs/home.nix
+++ dot_config/nixpkgs/home.nix
@@ -372,7 +372,7 @@ in
              PartOf = "graphical-session.target";
        };
        Service = {
-             ExecStart = "${pkgs.wl-clipboard}/bin/wl-paste -t text --watch clipman store --no-persist --max-items=200 --histpath=~/.local/share/clipman-primary.json";
+             ExecStart = "${pkgs.wl-clipboard}/bin/wl-paste -t text --watch clipman store --no-persist --max-items=1000 --histpath=~/.local/share/clipman-primary.json";
              Restart = "on-failure";
              RestartSec = 5;
        };
