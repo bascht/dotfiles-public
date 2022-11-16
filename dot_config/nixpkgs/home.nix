868dfe17cbca05b7e565ae8036e87f2d4c2d560d
Sebastian Schulze <sebastian.schulze@alfatraining.de>
2022-11-16 22:47:23 +0100

    [nix] Run thinkpad-dock for both timeout events (600 and 900)

--- dot_config/nixpkgs/home.nix
+++ dot_config/nixpkgs/home.nix
@@ -419,7 +419,7 @@ in
                before-sleep '${config.home.homeDirectory}/bin/blur-lock' \
                unlock '${config.home.homeDirectory}/bin/thinkpad-dock' \
                timeout 300 '${config.home.homeDirectory}/bin/blur-lock' \
-               timeout 600 '${pkgs.sway}/bin/swaymsg "output * dpms off"' resume '${pkgs.sway}/bin/swaymsg "output * dpms on"' \
+               timeout 600 '${pkgs.sway}/bin/swaymsg "output * dpms off"' resume '${pkgs.sway}/bin/swaymsg "output * dpms on" && /home/bascht/bin/thinkpad-dock' \
                timeout 900 '${config.home.homeDirectory}/bin/suspend-if-docked' resume '${pkgs.sway}/bin/swaymsg "output * dpms on" && /home/bascht/bin/thinkpad-dock'
              '';
      Restart = "on-failure";
