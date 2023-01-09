50e9e18797e8f1ed7f278dc6ecd51e1fab07868e
Sebastian Schulze <github.com@bascht.com>
2023-01-10 00:02:31 +0100

    [nixos] Experiment: Run node_exporter on the laptops and see how we fare

--- dot_config/nixos/thinkpads.nix
+++ dot_config/nixos/thinkpads.nix
@@ -12,5 +12,17 @@
   services.pipewire.enable = true;
   hardware.pulseaudio.enable = true;
   programs.sway.enable = true;
+
+  services.prometheus = {
+      exporters = {
+        node = {
+          enable = true;
+          enabledCollectors = [ "systemd" ];
+          port = 9100;
+        };
+      };
+    };
+    networking.firewall.extraCommands = "iptables -A nixos-fw -p tcp --source 10.11.12.10/32 --dport 9100:9100 -j nixos-fw-accept";
+    networking.firewall.extraStopCommands = "iptables -D nixos-fw -p tcp --source 10.11.12.10/32 --dport 9100:9100 -j nixos-fw-accept || true";
   };
 }
