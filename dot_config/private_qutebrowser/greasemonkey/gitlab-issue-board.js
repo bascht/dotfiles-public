34d42067372de3126c9b2c0da0caa172e9d8dc5f
Sebastian Schulze <sebastian.schulze@alfatraining.de>
2023-01-24 10:36:52 +0100

    [greasemonkey] Refactor GitLab userscript to show distinct flags

--- dot_config/private_qutebrowser/greasemonkey/gitlab-issue-board.js
+++ dot_config/private_qutebrowser/greasemonkey/gitlab-issue-board.js
@@ -3,9 +3,12 @@
 // @namespace   https://github.com/bascht
 // @description Adjust a few non-mono pages that I like to read
 // @include     /^https:\/\/git.*$/
+// @include     https://git.*
 // @include     https://gitlab.com*
 // @version     1
 // @author      Bascht
+// @grant       GM_xmlhttpRequest
+// @grant       GM.xmlHttpRequest
 // ==/UserScript==
 
 (async function IIFE() {
@@ -13,49 +16,80 @@
 
     const rtf = new Intl.RelativeTimeFormat("en-GB", {
         localeMatcher: "best fit",
-        numeric: "always",
+        // numeric: "always",
         style: "long",
     });
     var enhanceGitLabCard = async function(card) {
         const full_issue = $(card).find("h4 a").first().attr("href");
-        var project_path = full_issue.split("/-/issues/")[0].replace("https://git.alfa.sx/", "")
-        var issue_id = full_issue.split("/-/issues/")[1]
-        var full_api_path = "https://git.alfa.sx/api/v4/projects/" + encodeURIComponent(project_path) + "/issues/" + issue_id + "/related_merge_requests?per_page=100";
+        const origin = window.location.origin;
+        const project_path = full_issue.split("/-/issues/")[0].replace(origin + "/", "")
+        const issue_id = full_issue.split("/-/issues/")[1]
+        const full_api_path = origin + "/api/v4/projects/" + encodeURIComponent(project_path) + "/issues/" + issue_id + "/related_merge_requests?per_page=100";
 
         const board_info_items = $(card).find("span.board-info-items").first()
         GM.xmlHttpRequest({
             method: "GET",
             url: full_api_path,
             onload: function(response) {
+                $(board_info_items).css("width", "100%");
                 $(board_info_items).find("div.related-merge-requests").each(function(){ $(this).remove()});
                 $(JSON.parse(response.responseText)).each(function(){
-                    const div = $("<div>", { class: "related-merge-requests gl-display-flex align-items-start" })
-                    const a = $("<a>", { href: this.web_url, title: this.title })
+                    const div = $("<div>", { class: "related-merge-requests gl-display-flex align-items-start", style: "margin-bottom: 0.5em; margin-bottom: 0.5em;" })
+                    const item = $("<div>", { style: "display: flex; align-items: center; background-color: #d0e9ff; width: 100%; border-radius: 3px; padding: 0.1em", title: "(" + this.state + "/" + this.detailed_merge_status + ")" + this.title })
+                    const status_emoji = (function(mr) {
+                        switch(mr.state) {
+                        case "merged":
+                            const days_ago = Math.round((Date.parse(mr.merged_at) - new Date()) / 86400000);
 
-                    if(this.state == "merged") {
-                        const days_ago = Math.round((Date.parse(this.merged_at) - new Date()) / 86400000)
-                        var code = "<code>[✔️ " + rtf.format(days_ago, "days") +  "]</code>"
-                    }
-                    else
-                    {
-                        var code = "<code>[" + this.detailed_merge_status + "]</code>"
-                    }
-                    a.html(code + this.references.full)
-                    $(div).append(a)
-                    $(board_info_items).append(div)
+                            const merged_state = "🏁 ";
+
+                            if(days_ago == 0) {
+                                return merged_state + "today" ;
+                            } else if (days_ago == 1) {
+                                return merged_state + "yesterday";
+                            }
+
+                            return merged_state + rtf.format(days_ago, "day");
+
+                        case "opened":
+                            switch(mr.detailed_merge_status) {
+                            case "mergeable":
+                                return "👍";
+                            case "opened":
+                                return "🔧";
+                            case "not_approved":
+                                return "⌛";
+                            default:
+                                return mr.state;
+                            };
+                        };
+                    });
+
+                    item.append($("<span>", {style: "display: block; min-width: fit-content; font-size:0.8em"}).append(status_emoji(this)));
+                    item.append($("<a>", {href: this.web_url,  style: "display: block; margin-left: auto; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; line-height: 1rem; align-self: end; direction: rtl; text-align: left; padding: 0.1em;"}).append(this.references.full))
+
+                    $.each(this.reviewers, function(index, reviewer) {
+                        $(item).prepend($("<a>", { href: reviewer.web_url, class: "gl-link gl-avatar-link user-avatar-link js-no-trigger user-avatar-link" })
+                                        .append($("<span>").append($("<img>", { class: "gl-avatar gl-avatar-circle gl-avatar-s16 gl-lg-avatar-s24", src: reviewer.avatar_url} ))))
+                    });
+
+                    $(div).append(item)
+                    $(board_info_items).prepend(div)
 
                 })
             }
         });
     };
 
-    document.addEventListener('readystatechange', async function onReadyStateChange() {
-        $("body").on("click", function(e){
+    if (window.location.href.toString().includes('/boards/')) {
+        document.addEventListener('readystatechange', async function onReadyStateChange() {
+            $("body").on("click", function(e){
 
-            var card = $(e.target).parents("li[data-qa-selector='board_card']").first();
-            if(card) {
-                enhanceGitLabCard(card)
-            }
+                var card = $(e.target).parents("li[data-qa-selector='board_card']").first();
+                if(card.length > 0) {
+                    enhanceGitLabCard(card)
+                }
+            });
         });
-    });
+    };
 })();
