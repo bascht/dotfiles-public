f0b8322b9c6b2dceb10ea71c588a5285ae0521cf
Sebastian Schulze <zurek.dorhamm.me@bascht.com>
2023-06-01 17:26:35 +0200

    [nixos] Move more stuff into the futros module

--- dot_config/nixos/futros.nix
+++ dot_config/nixos/futros.nix
@@ -5,7 +5,19 @@
   config = {
     boot.initrd.availableKernelModules = [ "r8169"  "nvme" "ehci_pci" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" "amdgpu" ];
     boot.initrd.kernelModules = [ "dm-snapshot" "r8169"];
+    boot.kernelModules = [ "kvm-intel" "r8169" ];
 
+    services.prometheus = {
+      exporters = {
+        node = {
+          enable = true;
+          enabledCollectors = [ "systemd" ];
+          port = 9100;
+        };
+      };
+    };
+
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   };
 }
