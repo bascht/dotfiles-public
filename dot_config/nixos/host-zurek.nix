38c39d8fc0fa362039741b3355d44c8c731d225b
Sebastian Schulze <zurek.dorhamm.me@bascht.com>
2023-06-01 17:33:17 +0200

    [nixos] Add distinct file for zurek

---
+++ dot_config/nixos/host-zurek.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [];
+
+  config = {
+    networking.hostName = "zurek";
+
+    boot.initrd.preLVMCommands = lib.mkOrder 400 "sleep 1";
+
+    systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
+    virtualisation.podman.defaultNetwork.dnsname.enable = true;
+
+    networking = {
+      firewall.allowedTCPPorts = [ 22 53 80 81 443 9100 ];
+      firewall.allowedUDPPorts = [ 53 ];
+    };
+    fileSystems."/" =
+      {
+        device = "/dev/mapper/vg-root";
+        fsType = "ext4";
+      };
+
+    fileSystems."/var/storage" =
+      {
+        device = "/dev/mapper/chrupki";
+        fsType = "ext4";
+      };
+    fileSystems."/var/rost" =
+      {
+        device = "/dev/mapper/rost";
+        fsType = "ext4";
+      };
+
+    fileSystems."/boot" =
+      { device = "/dev/disk/by-uuid/353B-B671";
+        fsType = "vfat";
+      };
+
+    swapDevices =
+      [ { device = "/dev/mapper/vg-swap"; }
+      ];
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  };
+}
