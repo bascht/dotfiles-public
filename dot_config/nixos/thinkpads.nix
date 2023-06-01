e75190ffd9d7ae7b7a0363fa3249b779bf95940f
Sebastian Schulze <github.com@bascht.com>
2023-06-01 11:46:16 +0200

    [nix] Metrics host moved to new Fujitsu host

--- dot_config/nixos/thinkpads.nix
+++ dot_config/nixos/thinkpads.nix
@@ -27,7 +27,7 @@
         };
       };
     };
-    networking.firewall.extraCommands = "iptables -A nixos-fw -p tcp --source 10.11.12.10/32 --dport 9100:9100 -j nixos-fw-accept";
-    networking.firewall.extraStopCommands = "iptables -D nixos-fw -p tcp --source 10.11.12.10/32 --dport 9100:9100 -j nixos-fw-accept || true";
+    networking.firewall.extraCommands = "iptables -A nixos-fw -p tcp --source 10.11.12.20/32 --dport 9100:9100 -j nixos-fw-accept";
+    networking.firewall.extraStopCommands = "iptables -D nixos-fw -p tcp --source 10.11.12.20/32 --dport 9100:9100 -j nixos-fw-accept || true";
   };
 }
