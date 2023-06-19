f2fe4c41d0b0700a189ed353014fe3aa85ec6082
Sebastian Schulze <sebastian.schulze@alfatraining.de>
2023-06-19 18:22:14 +0200

    [nix] Use mu4e-development branch of evil-collections

--- dot_doom.d/packages.el
+++ dot_doom.d/packages.el
@@ -14,6 +14,11 @@
 (package! emacsql-sqlite-builtin :built-in 'prefer)
 (package! org-modern :built-in 'prefer)
 
+;; Until https://github.com/doomemacs/doomemacs/issues/7196 is fixed:
+(unpin! evil-collection)
+(package! evil-collection
+  :recipe (:repo "kepi/evil-collection" :branch "mu4e-development"))
+
 (when (string= (system-name) "apfelstrudel")
   (package! protobuf-mode)
   (package! salt-mode)
