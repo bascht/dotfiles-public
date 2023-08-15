5f0059e44bd6a953f7613765d7653efaf426c615
Sebastian Schulze <sebastian.schulze@alfatraining.de>
2023-08-15 10:33:27 +0200

    [emacs] Add dictcc package to load path

--- dot_doom.d/packages.el
+++ dot_doom.d/packages.el
@@ -15,6 +15,7 @@
 (package! pdf-tools :built-in 'prefer)
 (package! emacsql-sqlite-builtin :built-in 'prefer)
 (package! org-modern :built-in 'prefer)
+(package! dictcc :built-in 'prefer)
 
 ;; Until https://github.com/doomemacs/doomemacs/issues/7196 is fixed:
 (unpin! evil-collection)
