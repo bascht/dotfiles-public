09e1f231d5058524e798202f6bc671bd263fd58b
Sebastian Schulze <sebastian.schulze@alfatraining.de>
2023-09-12 10:39:56 +0200

    [emacs] Use dooms custom-set-faces! to style avy jumpers

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -58,12 +58,14 @@
       :desc "Open URL link"
       "o u" #'link-hint-open-link)
 
+
 (setq doom-modeline-time nil)
 (setq doom-modeline-persp-name t)
 (setq doom-modeline-modal nil)
 
 ; Make avy leads real nice big and shiny
-(face-spec-set 'avy-lead-face '((t (:inherit isearch :family "IBM Plex Mono" :background "deep pink" :foreground "snow"))))
+(custom-set-faces!
+  '(avy-lead-face :inherit isearch :family doom-font :bold t :italic nil :background "deep pink" :height 1.0 :foreground "snow"))
 
 (defun doom-dashboard-draw-ascii-banner-fn ()
   (let* ((banner
