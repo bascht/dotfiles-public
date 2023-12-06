9c1ad8423f580c08ac409028884536927e604fbd
Sebastian Schulze <github.com@bascht.com>
2023-12-07 00:29:40 +0100

    [nixos] Oh. And that flake.nix. I need a better process for chezmoi -.-

--- dot_config/nixos/flake.nix
+++ dot_config/nixos/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   inputs.home-manager = {
-    url = "github:nix-community/home-manager/release-23.05";
+    url = "github:nix-community/home-manager/release-23.11";
     inputs.nixpkgs.follows = "nixpkgs";
   };
   inputs.bascht-private = {
